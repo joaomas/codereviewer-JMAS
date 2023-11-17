46a0299ea85622b10b1deca87ea819afe4951c76
deniseiras <denis.eiras@gmail.com>
2023-11-17 16:33:22 -0300

    empty method for generating xml files

--- src/Parser_XML.py
+++ src/Parser_XML.py
@@ -5,8 +5,16 @@ class Parser_XML(Parser):
     def __init__(self) -> None:
         self.xml_struct
 
-    
+
+    def generate_xml(self):
+        # TODO
+        # Generate all xmlfiles using open-fortran-parser like createXML.bash
+        # open_fortran_parser ./code_under_test/teste.F90 ./xml/saida.xml
+        pass
+        
+        
     def parse(self): # TODO create XML_struct to return:
+        
         
         # xml_structure = Create struct  = root ?
         # TODO
