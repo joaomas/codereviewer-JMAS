821dd0c0917d8dcf02d7855f18223a353b688881
deniseiras <denis.eiras@gmail.com>
2023-12-14 16:57:51 -0300

    inserting test for Parser that just fails

---
+++ tests/parser_xml_test.py
@@ -0,0 +1,11 @@
+# import src.Parser_XML  
+import unittest  
+
+class Test_TestIncrementDecrement(unittest.TestCase):
+
+    def test_parse_xml_happy_day(self):
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
